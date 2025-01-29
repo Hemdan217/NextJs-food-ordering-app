13658a39b01d6fa6923f2dc6f1ac6fe098572cfb
Mr Robot <hemdan219@gmail.com>
2025-01-29 21:57:03 +0200

    initialize Next.js project with TypeScript, Tailwind CSS, and ESLint configuration

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section className="container grid md:grid-cols-2 grid-cols-1 gap-4">
+      <div>This is test</div>
+      <div>This is test</div>
+    </section>
+  );
+};
+
+export default Hero;
